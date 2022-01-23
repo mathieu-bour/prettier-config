a4420c52922dcd060fb57fd696f5c73f2bb00e24
Mathieu Bour <mathieu.tin.bour@gmail.com>
2022-01-23 12:00:54 +0100

    feat(solidity): use `printWidth: 120` and `bracketSpacing`
    release v1.1.0

--- .prettierrc.js
+++ .prettierrc.js
@@ -27,11 +27,11 @@ module.exports = {
     {
       files: '*.sol',
       options: {
-        printWidth: 80,
-        tabWidth: 4,
+        printWidth: 120,
+        tabWidth: 2,
         useTabs: false,
         singleQuote: false,
-        bracketSpacing: false,
+        bracketSpacing: true,
         explicitTypes: 'always',
       },
     },
